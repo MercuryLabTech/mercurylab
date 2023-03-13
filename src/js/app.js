048575b65f44623488e5d5de958e7fe5982689ad
JasurAvazov <jasuravazov4@gmail.com>
2023-03-13 17:08:18 +0500

    footer

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,17 @@
 import barba from '@barba/core';
 
+// modules
 import * as functions from "./modules/functions.js";
 
+// components
+import * as footer from "./components/footer.js";
+
+// modules
 functions.isWebp();
+
+// components
+footer.init();
+
 
 barba.init({
     sync: true,
