ff04cf606743987c864e3172aff849365dc1a3f0
JasurAvazov <jasuravazov4@gmail.com>
2023-03-13 18:11:46 +0500

    locomotive scroll

---
+++ src/js/modules/locomotive.js
@@ -0,0 +1,10 @@
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('body'),
+    smooth: true,
+    multiplier: 0.5,
+    smartphone: {
+        smooth: true
+    }
+});
