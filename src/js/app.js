0f4c56685545dcbd672aeff8ab72e5469b927c4f
shahrizoda <shahrizodadeveloping@gmail.com>
2023-03-15 16:12:22 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -21,6 +21,25 @@ telegramSend.init();
 footer.init();
 popup.init();
 
+
+$(document).ready(function() {
+    $(".accordion > .accordion__button").on("click", function() {
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+        $(this)
+          .siblings(".accordion__content")
+          .slideUp(200);
+      } else {
+        $(".accordion > .accordion__button").removeClass("active");
+        $(this).addClass("active");
+        $(".accordion__content").slideUp(200);
+        $(this)
+          .siblings(".accordion__content")
+          .slideDown(200);
+      }
+    });
+});
+
 function pageTransition() {
     var tl = gsap.timeline();
     tl.to('.transition li', {duration: .5, scaleY: 1, transformOrigin: "bottom left", stagger: .2})
@@ -53,3 +72,4 @@ barba.init({
         },
     }]
 })
+
