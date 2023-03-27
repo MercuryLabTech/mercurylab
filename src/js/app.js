9b47df962407716dd2200e72c004f9c370a1df25
shahriyor-sharifjonov <shahriyordeveloping@gmail.com>
2023-03-27 21:54:49 +0500

    fixed barba bugs

--- src/js/app.js
+++ src/js/app.js
@@ -1,46 +1,16 @@
 import barba from '@barba/core';
-import gsap from 'gsap';
-import $ from 'jquery';
 
 // modules
 import * as functions from "./modules/functions.js";
 import * as telegramSend from "./modules/telegramSend.js";
 import * as scroll from "./modules/scroll.js";
 import * as animation from "./modules/animations.js";
+import * as accordion from "./modules/accordion.js";
 
 // components
 import * as footer from "./components/footer.js";
 import * as popup from "./components/popup.js";
 import * as reviews from "./components/reviews.js"
-
-// modules
-functions.isWebp();
-scroll.init();
-animation.init();
-telegramSend.init();
-
-// components
-footer.init();
-popup.init();
-reviews.init();
-
-$(document).ready(function() {
-    $(".accordion > .accordion__button").on("click", function() {
-      if ($(this).hasClass("active")) {
-        $(this).removeClass("active");
-        $(this)
-          .siblings(".accordion__content")
-          .slideUp(200);
-      } else {
-        $(".accordion > .accordion__button").removeClass("active");
-        $(this).addClass("active");
-        $(".accordion__content").slideUp(200);
-        $(this)
-          .siblings(".accordion__content")
-          .slideDown(200);
-      }
-    });
-});
 
 function delay(n) {
     n = n || 2000;
@@ -53,6 +23,13 @@ function delay(n) {
 
 barba.init({
     sync: true,
+    views: [{
+        namespace: 'home',
+        beforeEnter() {
+            reviews.init();
+            accordion.init();
+        },
+    }],
     transitions: [{
         async leave(data) {
             const done = this.async();
@@ -69,7 +46,14 @@ barba.init({
             document.querySelector('.transition li').classList.remove('enter');
         },
         async once(data) {
-            // gsap.to('.transition li', {duration: 1, clip: "polygon(0 100%, 100% 100%, 100% 100%, 0 100%)"})
+            reviews.init();
+            functions.isWebp();
+            scroll.init();
+            animation.init();
+            telegramSend.init();
+            footer.init();
+            popup.init();
+            accordion.init();
         },
     }]
 })
--- src/js/components/reviews.js
+++ src/js/components/reviews.js
@@ -1,6 +1,7 @@
 import Swiper from "swiper";
 
 export function init() {
+    console.log('reviews init');
     var swiper = new Swiper(".swiper", {
         slidesPerView: "auto",
         spaceBetween: 30,
