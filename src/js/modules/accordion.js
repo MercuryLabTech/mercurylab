453e692432bdc15f7b541de98f86b69c92fc8476
shahriyor-sharifjonov <shahriyordeveloping@gmail.com>
2023-03-27 23:51:51 +0500

    changed accordion

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -3,19 +3,21 @@ import $ from "jquery";
 export function init() {
     $(document).ready(function() {
         $(".accordion > .accordion__button").on("click", function() {
-        if ($(this).hasClass("active")) {
-            $(this).removeClass("active");
-            $(this)
-            .siblings(".accordion__content")
-            .slideUp(200);
-        } else {
-            $(".accordion > .accordion__button").removeClass("active");
-            $(this).addClass("active");
-            $(".accordion__content").slideUp(200);
-            $(this)
-            .siblings(".accordion__content")
-            .slideDown(200);
-        }
+            if ($(this).hasClass("active")) {
+                $(this).removeClass("active");
+                $(this)
+                    .siblings(".accordion__content")
+                    .slideUp(600);
+            } else {
+                $(".accordion__content").slideUp(600);
+                setTimeout(() => {
+                    $(this)
+                        .siblings(".accordion__content")
+                        .slideDown(500);
+                }, document.querySelector('.accordion__button.active') ? 600 : 0);
+                $(".accordion > .accordion__button").removeClass("active");
+                $(this).addClass("active");
+            }
         });
     });
 }
